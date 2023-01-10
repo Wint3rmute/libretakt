52cb49a23ba5e6b1b582501c3139ac788f1a3196
wint3rmute <mateusz.baczek1998@gmail.com>
2023-01-10 14:58:16 +0100

    Server listen on 0.0.0.0

--- server/src/main_server.rs
+++ server/src/main_server.rs
@@ -29,7 +29,7 @@ pub async fn main() -> std::io::Result<()> {
             .service(hello)
             .data(chat_server.clone()) //register the lobby
     })
-    .bind("127.0.0.1:8080")
+    .bind("0.0.0.0:8080")
     .unwrap()
     .run()
     .await
