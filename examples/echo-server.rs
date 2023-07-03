1b38f3e0c04f8ce7472885212f0ea52298702b14
baczek <m.baczek@satrev.space>
2023-07-03 16:29:36 +0200

    adding websockets to egui

--- examples/echo-server.rs
+++ examples/echo-server.rs
@@ -22,7 +22,7 @@ async fn main() -> Result<(), Error> {
     let _ = env_logger::try_init();
     let addr = env::args()
         .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+        .unwrap_or_else(|| "127.0.0.1:8081".to_string());
 
     // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&addr).await;
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,11 +1,63 @@
 use egui::Align;
+use ewebsock::{WsEvent, WsMessage, WsReceiver, WsSender};
 use log::info;
 
-pub struct LibretaktUI {}
+struct WebSocketConnection {
+    ws_sender: WsSender,
+    ws_receiver: WsReceiver,
+    events: Vec<WsEvent>,
+    text_to_send: String,
+}
+
+impl WebSocketConnection {
+    fn new(ws_sender: WsSender, ws_receiver: WsReceiver) -> Self {
+        Self {
+            ws_sender,
+            ws_receiver,
+            events: Default::default(),
+            text_to_send: Default::default(),
+        }
+    }
+
+    fn ui(&mut self, ctx: &egui::Context) {
+        while let Some(event) = self.ws_receiver.try_recv() {
+            self.events.push(event);
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.label("Message to send:");
+                if ui.text_edit_singleline(&mut self.text_to_send).lost_focus()
+                    && ui.input(|i| i.key_pressed(egui::Key::Enter))
+                {
+                    info!("wololo");
+                    self.ws_sender
+                        .send(WsMessage::Text(std::mem::take(&mut self.text_to_send)));
+                }
+            });
+
+            ui.separator();
+            ui.heading("Received events:");
+            for event in &self.events {
+                ui.label(format!("{:?}", event));
+            }
+        });
+    }
+}
+
+pub struct LibretaktUI {
+    server_url: String,
+    websocket: Option<WebSocketConnection>,
+    error: Option<String>,
+}
 
 impl Default for LibretaktUI {
     fn default() -> Self {
-        Self {}
+        Self {
+            server_url: "http://localhost:8081".to_string(),
+            websocket: None,
+            error: None,
+        }
     }
 }
 
@@ -16,6 +68,20 @@ impl LibretaktUI {
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
         Default::default()
+    }
+
+    fn connect(&mut self, ctx: egui::Context) {
+        let wakeup = move || ctx.request_repaint(); // wake up UI thread on new message
+        match ewebsock::connect_with_wakeup(&self.server_url, wakeup) {
+            Ok((ws_sender, ws_receiver)) => {
+                self.websocket = Some(WebSocketConnection::new(ws_sender, ws_receiver));
+                self.error = None;
+            }
+            Err(error) => {
+                log::error!("Failed to connect to {:?}: {}", &self.server_url, error);
+                self.error = Some(error);
+            }
+        }
     }
 }
 
