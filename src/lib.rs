ca51ec9a2b6f53d162f6a37492f2e80bfbe6e4d2
baczek <m.baczek@satrev.space>
2023-07-03 13:54:58 +0200

    Integrate eframe into project

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 //! Collaborative sampler!
 
+#[cfg(not(target_arch = "wasm32"))]
 pub mod constants;
+#[cfg(not(target_arch = "wasm32"))]
 pub mod engine;
+#[cfg(not(target_arch = "wasm32"))]
 pub mod persistence;
+#[cfg(not(target_arch = "wasm32"))]
 pub mod sample_provider;
+#[cfg(not(target_arch = "wasm32"))]
 pub mod sequencer;
-pub mod ui_skins;
