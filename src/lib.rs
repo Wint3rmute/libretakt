1ee4c10142e6c283d96ac853f9126ff1574cfcf4
wint3rmute <mateusz.baczek1998@gmail.com>
2023-07-17 14:44:03 +0200

    Clickable sequencer grid

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@ pub mod constants;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod engine;
 #[cfg(not(target_arch = "wasm32"))]
-pub mod persistence;
-#[cfg(not(target_arch = "wasm32"))]
 pub mod sample_provider;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod sequencer;
 
+#[cfg(target_arch = "wasm32")]
+pub mod sequencer;
 #[cfg(target_arch = "wasm32")]
 pub mod ui;
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,3 +1,4 @@
+use crate::sequencer::Sequencer;
 use egui::Align;
 use egui::Direction;
 use ewebsock::{WsEvent, WsMessage, WsReceiver, WsSender};
@@ -31,6 +32,7 @@ pub struct LibretaktUI {
     server_url: String,
     websocket: Option<WebSocketConnection>,
     error: Option<String>,
+    sequencer: Sequencer,
 }
 
 impl Default for LibretaktUI {
@@ -39,6 +41,7 @@ impl Default for LibretaktUI {
             server_url: "http://localhost:8081".to_string(),
             websocket: None,
             error: None,
+            sequencer: Sequencer::default(),
         }
     }
 }
@@ -68,7 +71,7 @@ impl LibretaktUI {
 }
 
 impl eframe::App for LibretaktUI {
-    fn add_step(ui: egui::Ui) {}
+    // fn add_step(ui: egui::Ui) {}
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::TopBottomPanel::bottom("bottom_menu").show(ctx, |ui| {
@@ -91,26 +94,52 @@ impl eframe::App for LibretaktUI {
         });
 
         egui::TopBottomPanel::top("my_panel").show(ctx, |ui| {
-            ui.label("Hello World! From `TopBottomPanel`, that must be before `CentralPanel`!");
+            ui.label("CONNECTED: OFFLINE, JAMMERS: 1");
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             let width = ui.min_size().x / 4.0;
             let height = 60.0;
 
+            ui.label("SEQ");
+
             egui::Grid::new("some_unique_id")
                 .spacing(egui::Vec2 { x: 0.0, y: 0.0 })
                 .show(ui, |ui| {
-                    ui.add_sized([width, height], egui::widgets::Button::new("asasdsa"));
-                    ui.add_sized([width, height], egui::widgets::Button::new("asasdsa"));
-                    ui.add_sized([width, height], egui::widgets::Button::new("asasdsa"));
-                    ui.add_sized([width, height], egui::widgets::Button::new("asasdsa"));
-                    ui.end_row();
-                    ui.add_sized([width, height], egui::widgets::Button::new("asasdsa"));
-                    ui.add_sized([width, height], egui::widgets::Button::new("asasdsa"));
-                    ui.add_sized([width, height], egui::widgets::Button::new("asasdsa"));
-                    ui.add_sized([width, height], egui::widgets::Button::new("asasdsa"));
-                    ui.end_row();
+                    let current_track = &mut self.sequencer.tracks[0];
+                    for (step_num, step) in current_track.steps.iter_mut().enumerate() {
+                        if step_num != 0 && step_num % 4 == 0 {
+                            ui.end_row();
+                        }
+
+                        let step_text = egui::widget_text::RichText::new(format!("{step_num}"))
+                            .color(if step.set {
+                                egui::Color32::LIGHT_RED
+                            } else {
+                                egui::Color32::WHITE
+                            });
+
+                        if ui
+                            .add_sized(
+                                [width, height],
+                                egui::widgets::Button::new(step_text).fill(
+                                    if current_track.current_step_num == step_num {
+                                        egui::Color32::DARK_GRAY
+                                    } else {
+                                        egui::Color32::TRANSPARENT
+                                    },
+                                ),
+                            )
+                            .clicked()
+                        {
+                            step.set = !step.set;
+                            if step.set {
+                                info!("step {step_num} set");
+                            } else {
+                                info!("step {step_num} unset");
+                            }
+                        }
+                    }
                 });
         });
 
